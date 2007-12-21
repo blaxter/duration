31fe1d7f33287ee75bdd54c09081d4cb304e3b03
kuja <kuja@fed8fec5-6314-0410-9670-8f8ba00fed7e>
2007-12-21 14:20:15 +0000

    added some documentation for the english locale
    git-svn-id: http://duration.rubyforge.org/svn/trunk@37 fed8fec5-6314-0410-9670-8f8ba00fed7e

--- lib/duration/localizations/english.rb
+++ lib/duration/localizations/english.rb
@@ -8,6 +8,12 @@ class Duration
       FORMAT    = proc do |duration|
     		str = duration.format('%w %~w, %d %~d, %h %~h, %m %~m, %s %~s')
     		str.sub(/^0 [a-z]+,?/i, '').gsub(/ 0 [a-z]+,?/i, '').chomp(',').sub(/, (\d+ [a-z]+)$/i, ' and \1').strip
+    		# Produces a message like:
+    		#  10 weeks, 2 days, 3 hours, 4 minutes and 5 seconds
+    		#
+    		# Any zero values should be removed.
+    		# For example, if there's 0 days and 0 minutes, it might be:
+    		#   10 weeks, 3 hours and 5 seconds
     	end
     end
   end
